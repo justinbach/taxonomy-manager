785588ce8e37d050d2ccd0d51d271d31639805dd
Justin Bachorik <justinbach@gmail.com>
2014-07-01 16:42:46 -0400

    added initial layout files using bootstrap

---
+++ app/views/welcome.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('content')
+    <div class="welcome-block">
+        <h1>Welcome to the NPR Taxonomy Manager</h1>
+        <p class="lead">Sign up or sign in to begin managing taxonomies and terms.</p>
+    </div>
+@stop
