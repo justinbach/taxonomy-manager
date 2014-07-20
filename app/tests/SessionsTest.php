9b552399b337c140983b39868dbaa9a0e652f045
Justin Bachorik <justinbach@gmail.com>
2014-07-20 09:32:23 -0400

    moved DB migration and seeding setup into parent test class

--- app/tests/SessionsTest.php
+++ app/tests/SessionsTest.php
@@ -6,8 +6,6 @@ class SessionsTest extends TestCase
     public function setUp()
     {
         parent::setUp();
-        Artisan::call('migrate');
-        $this->seed();
         Session::start();
         Route::enableFilters();
     }
