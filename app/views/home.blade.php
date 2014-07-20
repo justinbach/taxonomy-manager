7b9b2fe2839d83f4f7499f28be2ff347dc7cc15f
Justin Bachorik <justinbach@gmail.com>
2014-07-20 11:07:32 -0400

    Added quick link to home page

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -2,4 +2,5 @@
 
 @section('content')
 <h1>Welcome, {{ Auth::user()->name }}!</h1>
+<p>Get started by editing some <a href="{{ URL::action('TaxonomiesController@index') }}">taxonomies</a>.</p>
 @stop
